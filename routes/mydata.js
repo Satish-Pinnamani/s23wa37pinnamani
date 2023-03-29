829a8149a6a1bfc4c0f7eda7686d06a265dbea54
Satish-Pinnamani <pinnamani97@gmail.com>
2023-03-29 18:32:51 -0500

    added user end point

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-let sum = 0;
-let more = 0;
-
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  more = more + 1;
-  sum = sum + more;
-  res.send(`sum is : ${sum}`)
+  res.render('mydata', { title: 'Purna Chandra Satish Pinnamani ' });
 });
 
 module.exports = router;
